083023d4ca4f8b02f4c1c01db9f3d72752dec150
KiraTeresa <kira.soba@gmx.de>
2024-10-05 18:59:20 +0200

    basic global styles with examples (closes #1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import classes from './homepage.module.scss';
+
 export default function HomePage() {
   return (
-    <div>This is the Homepage of MindSet, will only be needed for login.</div>
+    <>
+      <div>
+        This is the Homepage of MindSet, will only be needed for login.
+        <div className={classes.small}>nested div</div>
+      </div>
+      <p className={classes.small}>extra small</p>
+      <p className={classes.regular}>small</p>
+      <p className={classes.medium}>medium</p>
+      <p className={classes.large}>large</p>
+      <p className={classes.extraLarge}>extra large</p>
+    </>
   );
 }
